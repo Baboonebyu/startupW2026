fc225d2c4a2570dd35fad4663c2da025027c9f65
Benjamin <baboone@byu.edu>
2026-02-05 16:04:10 -0700

    Got the basics fpr game.jsx done

--- src/game/game.jsx
+++ src/game/game.jsx
@@ -1,9 +1,50 @@
 import React from 'react';
+import './gameStyle.css';
 
 export function Game() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>game displayed here</div>
+    <main className="container-fluid  text-center">
+              <div className ="websocketInfo">
+<p> websocketstuff</p>
+<p>player gets high score</p>
+
+</div>
+        <div className="gameBoard">
+            <div className = "card"> <img src="Temple photo library/bountiful.jpg" alt="bountiful" width="100%" height="100%"/></div>
+            <div className = "card"> <img src="Temple photo library/brigham city.jpg" alt="brigham city" width="100%" height="100%"/></div>
+            <div className = "card"> <p>Bountiful</p></div>
+            <div className = "card"> <p>Brigham City</p></div>
+
+            <div className = "card"> <img src="Temple photo library/layton.jpg" alt="layton" width="100%" height="100%"/></div>
+            <div className = "card"> <img src="Temple photo library/logan.jpg" alt="logan" width="100%" height="100%"/></div>
+            <div className = "card"> <p>Layton</p></div>
+            <div className = "card"> <p>Logan</p></div>
+
+            <div className = "card"> <img src="Temple photo library/saltlake.jpg" alt="salt lake" width="100%" height="100%"/></div>
+            <div className = "card"> <img src="Temple photo library/ogden.jpg" alt="ogden" width="100%" height="100%"/></div>
+            <div className = "card"> <p>Salt Lake</p></div>
+            <div className = "card"> <p>Ogden</p></div>
+
+            <div className = "card"> <img src="Temple photo library/smithfield.jpg" alt="smithfield" width="100%" height="100%"/></div>
+            <div className = "card"> <img src="Temple photo library/syracuse.jpg" alt="syracuse" width="100%" height="100%"/></div>
+            <div className = "card"> <p>Smithfield</p></div>
+            <div className = "card"> <p>Syracuse</p></div>
+        
+
+
+
+
+        </div>
+         <div class ="mobileHolder">
+
+      <div class ="websocketInfoMobile">
+<p> websocketstuff</p>
+<p>player gets high score</p>
+
+</div>
+    </div>
+
+
     </main>
   );
 }
