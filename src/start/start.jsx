0cca66188b821335c053400eb27e766db59316bf
Benjamin <baboone@byu.edu>
2026-02-05 16:27:04 -0700

    set up start.jsx

--- src/start/start.jsx
+++ src/start/start.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
-
+import './start.css';
 export function Play() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>Play displayed here</div>
+    <main className="container-fluid  text-center">
+      
+    <div class="box">
+
+
+    <h1>
+        Ready to
+        Match?
+    </h1>
+    
+    <a href="game" class="btn btn-primary btn-lg" role="button" aria-pressed="true">Let's go!</a>
+
+
+
+
+    </div>
+
     </main>
   );
 }
